17dcec1e23cc2643de6ec394c3667471646667aa
dev-timm <timm.kuemmeth@gmail.com>
2024-06-29 16:09:10 +0200

    add navbar with menu toggle

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,11 @@
-console.log('hello');
+const sidebar = document.querySelector('.sidebar');
+const sidebarOpen = document.querySelector('.sidebar-open');
+const sidebarClose = document.querySelector('.sidebar-close');
+
+sidebarOpen.addEventListener('click', () => {
+  sidebar.classList.add('show-sidebar');
+});
+
+sidebarClose.addEventListener('click', () => {
+  sidebar.classList.remove('show-sidebar');
+});
