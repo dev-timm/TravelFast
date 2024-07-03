cce9b558a6e0a2f5b2675ddebdf1ac8048f7e5ca
dev-timm <timm.kuemmeth@gmail.com>
2024-07-03 16:11:58 +0200

    auto-genrerate available cities in sidebar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 import cities from './data.js';
 
 const sidebar = document.querySelector('.sidebar');
+const sidebarList = document.querySelector('.sidebar-list');
 const sidebarOpen = document.querySelector('.sidebar-open');
 const sidebarClose = document.querySelector('.sidebar-close');
 
@@ -11,3 +12,18 @@ sidebarOpen.addEventListener('click', () => {
 sidebarClose.addEventListener('click', () => {
   sidebar.classList.remove('show-sidebar');
 });
+
+const sidebarData = cities
+  .map((city) => {
+    return `
+        <li>
+          <a href="#" class="sidebar-item">
+            <p>${city.name}</p>
+            <img src="./assets/images/chevron-right.svg" alt="chevron" />
+          </a>
+        </li>
+        `;
+  })
+  .join('');
+
+sidebarList.innerHTML = sidebarData;
