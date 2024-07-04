6392ec9b87f3c287fc03b8f8fee890a8c2097ca1
dev-timm <timm.kuemmeth@gmail.com>
2024-07-04 16:38:52 +0200

    display city information after clicking on sidebar item

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,7 @@ const sidebar = document.querySelector('.sidebar');
 const sidebarList = document.querySelector('.sidebar-list');
 const sidebarOpen = document.querySelector('.sidebar-open');
 const sidebarClose = document.querySelector('.sidebar-close');
+const cityDOM = document.querySelector('.city-info');
 
 sidebarOpen.addEventListener('click', () => {
   sidebar.classList.add('show-sidebar');
@@ -13,11 +14,11 @@ sidebarClose.addEventListener('click', () => {
   sidebar.classList.remove('show-sidebar');
 });
 
-const sidebarData = cities
+const sidebarCity = cities
   .map((city) => {
     return `
         <li>
-          <a href="#" class="sidebar-item">
+          <a href="#" class="sidebar-city" data-id="${city.name}">
             <p>${city.name}</p>
             <img src="./assets/images/chevron-right.svg" alt="chevron" />
           </a>
@@ -26,4 +27,39 @@ const sidebarData = cities
   })
   .join('');
 
-sidebarList.innerHTML = sidebarData;
+sidebarList.innerHTML = sidebarCity;
+
+const displayCity = (allCities) => {
+  let singleCity = allCities.map((city) => {
+    return `<h1 class="city-title">${city.name}</h1>
+        <hr />
+        <p class="city-desc">
+          ${city.desc}
+        </p>
+        <div class="actions">
+          <a href="${city.booking}" class="primary-cta">Wanna Go?</a>
+          <a href="${city.info}" class="secondary-cta">
+            Get more info
+            <img
+              class="link-arrow"
+              src="./assets/images/arrow-right.svg"
+              alt="arrow right"
+          /></a>
+        </div>`;
+  });
+  cityDOM.innerHTML = singleCity;
+};
+
+const sidebarCities = document.querySelectorAll('.sidebar-city');
+
+sidebarCities.forEach((city) => {
+  city.addEventListener('click', (e) => {
+    const selectedCity = e.currentTarget.dataset.id;
+    const cityContent = cities.filter((cityItem) => {
+      if (cityItem.name === selectedCity) {
+        return cityItem;
+      }
+    });
+    displayCity(cityContent);
+  });
+});
